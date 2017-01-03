768cbb5a787f33b047c5c7f357a77e12da4dfa03
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-01-03 02:57:09 +0200

    Fix home work: update.

--- test/com/urise/webapp/storage/ArrayStorageTest.java
+++ test/com/urise/webapp/storage/ArrayStorageTest.java
@@ -4,7 +4,7 @@ package com.urise.webapp.storage;
  * Created by Andrey on 02.01.2017.
  */
 public class ArrayStorageTest extends AbstractArrayStorageTest {
-    public ArrayStorageTest(ArrayStorage storage) {
-        super(storage);
+    public ArrayStorageTest() {
+        super(new ArrayStorage());
     }
 }
--- test/com/urise/webapp/storage/SortedArrayStorageTest.java
+++ test/com/urise/webapp/storage/SortedArrayStorageTest.java
@@ -4,7 +4,7 @@ package com.urise.webapp.storage;
  * Created by Andrey on 02.01.2017.
  */
 public class SortedArrayStorageTest extends AbstractArrayStorageTest{
-    public SortedArrayStorageTest(SortedArrayStorage storage) {
-        super(storage);
+    public SortedArrayStorageTest() {
+        super(new SortedArrayStorage());
     }
 }
