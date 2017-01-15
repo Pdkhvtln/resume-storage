ece9c62fcf151d174a16ab765613e666af08840f
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-01-16 00:15:25 +0200

    Implementation Comparable into Resume replaced anonymous class Comparator into SortedArrayStorage.

--- src/com/urise/webapp/storage/SortedArrayStorage.java
+++ src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -3,16 +3,23 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 /**
  * Created by Andrey on 28.12.2016.
  */
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+    private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>() {
+        @Override
+        public int compare(Resume o1, Resume o2) {
+            return o1.getUuid().compareTo(o2.getUuid());
+        }
+    };
 
     @Override
     protected Integer getSearchKey(String uuid) {
-        return Arrays.binarySearch(storage, 0, size, new Resume(uuid));
+        return Arrays.binarySearch(storage, 0, size, new Resume(uuid), RESUME_COMPARATOR);
     }
 
     @Override
