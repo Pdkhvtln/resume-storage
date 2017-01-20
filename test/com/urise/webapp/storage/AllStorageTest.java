74bcad364610d6456cb4136cd55ac98f201ba7ca
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-01-20 12:08:49 +0200

    Introduce new classAllStorageTest of JUnit.

---
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -0,0 +1,19 @@
+package com.urise.webapp.storage;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Andrey on 20.01.2017.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArrayStorageTest.class,
+        ListStorageTest.class,
+        MapFullNameStorageTest.class,
+        MapUuidStorageTest.class,
+        SortedArrayStorageTest.class
+
+})
+public class AllStorageTest {
+}
