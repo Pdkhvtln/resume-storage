7922735b8f0c7a8da4de06d9663e28bf8b6c010f
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-01-30 00:15:23 +0200

    Added Enum SectionType.

---
+++ src/com/urise/webapp/model/SectionType.java
@@ -0,0 +1,24 @@
+package com.urise.webapp.model;
+
+/**
+ * Created by andrew on 30.01.17.
+ */
+public enum SectionType {
+    PERSONAL("Личны качества"),
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+    private String title;
+
+    SectionType(String title)
+    {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
