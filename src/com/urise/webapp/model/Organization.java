a459eb509d4ea94e8062b2d6009de353e1a85c85
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-02-16 00:29:57 +0200

    List of Class Position is composite of class Organization.

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,6 +1,7 @@
 package com.urise.webapp.model;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Objects;
 
 /**
@@ -9,21 +10,12 @@ import java.util.Objects;
 public class Organization {
     private final Link homePage;
 
-    private final LocalDate startDate;
-    private final LocalDate endDate;
-    private final String title;
-    private final String description;
+    private final List<Position> position;
 
-    public Organization(String name, String url, LocalDate startDate, LocalDate endDate, String title, String description) {
+    public Organization(String name, String url, List<Position> position)//, LocalDate startDate, LocalDate endDate, String title, String description)
+    {
         this.homePage = new Link(name,url);
-        Objects.requireNonNull(startDate,"startDate must not be null");
-        Objects.requireNonNull(endDate,"endDate must not be null");
-        Objects.requireNonNull(title,"title must not be null");
-
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.title = title;
-        this.description = description;
+        this.position = position;
     }
 
     @Override
@@ -33,20 +25,14 @@ public class Organization {
 
         Organization that = (Organization) o;
 
-        if (!homePage.equals(that.homePage)) return false;
-        if (!startDate.equals(that.startDate)) return false;
-        if (!endDate.equals(that.endDate)) return false;
-        if (!title.equals(that.title)) return false;
-        return description != null ? description.equals(that.description) : that.description == null;
+        if (homePage != null ? !homePage.equals(that.homePage) : that.homePage != null) return false;
+        return position != null ? position.equals(that.position) : that.position == null;
     }
 
     @Override
     public int hashCode() {
-        int result = homePage.hashCode();
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
+        int result = homePage != null ? homePage.hashCode() : 0;
+        result = 31 * result + (position != null ? position.hashCode() : 0);
         return result;
     }
 
@@ -54,10 +40,7 @@ public class Organization {
     public String toString() {
         return "Organization{" +
                 "homePage=" + homePage +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
+                ", position=" + position +
                 '}';
     }
 }
