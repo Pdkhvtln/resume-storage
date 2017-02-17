fc0e5e38f5c87448871ff5ea4d2ff25f1afcf649
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-02-17 09:16:54 +0200

    Fix error in AbstractStoragTest and class Section selected three methods.

--- src/com/urise/webapp/model/Section.java
+++ src/com/urise/webapp/model/Section.java
@@ -4,5 +4,9 @@ package com.urise.webapp.model;
  * Created by andrew on 02.02.17.
  */
 public abstract class Section {
+    public abstract boolean equals(Object o);
 
+    public abstract int hashCode();
+
+    public abstract String toString();
 }
