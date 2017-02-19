56c2ab58152158598faf1d9345b4cd093459753a
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-02-20 00:12:51 +0200

    Print files/directories  in view of tree.

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -8,18 +8,18 @@ import java.io.IOException;
  */
 public class MainFile {
     public static void main(String[] args) throws IOException {
-        printDirectoryDeeply(new File("/home/andrew/IdeaProjects/resume-storage"));
+        printDirectoryDeeply(new File("/home/andrew/IdeaProjects/resume-storage"), " ");
 
     }
     //Сделать рекурсивный обход и вывод имени файлов в каталогах и подкаталогах (корневой каталог- ваш проект)
-    public static void printDirectoryDeeply(File dir) {
-        for (File f:dir.listFiles()) {
+
+    public static void printDirectoryDeeply(File dir, String tree) {
+        for (File f : dir.listFiles()) {
             if (f.isFile()) {
-                System.out.println("File: "+ f.getName());
-            } else if (f.isDirectory())
-            {
-                System.out.println("Directory: "+f.getName());
-                printDirectoryDeeply(f);
+                System.out.println(tree + "File: " + f.getName());
+            } else if (f.isDirectory()) {
+                System.out.println(tree + "Directory: " + f.getName());
+                printDirectoryDeeply(f, tree + "  ");
             }
         }
     }
