b4948f8bb874c1fd05ad872fd3a86e3e6ae376ff
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-03-17 20:03:42 +0200

    The part of JSTL. The files changed: Resume, ContactType, resumeServlet, style.css, lis.jsp and index.hml. The files added: edit.jsp and view.jsp.

--- src/com/urise/webapp/model/ContactType.java
+++ src/com/urise/webapp/model/ContactType.java
@@ -7,8 +7,18 @@ public enum ContactType {
     PHONE("Тел."),
     MOBILE("Мобильный"),
     HOME_PHONE("Домашний тел."),
-    SKYPE("Skype"),
-    MAIL("e-mail"),
+    SKYPE("Skype") {
+        @Override
+        public String toHtml0(String value) {
+            return "<a href='skype:" + value + "'>" + value + "</a>";
+        }
+    },
+    MAIL("e-mail") {
+        @Override
+        public String toHtml0(String value) {
+            return "<a href='mailto:" + value + "'>" + value + "</a>";
+        }
+    },
     LINKEDIN("Профиль LinkedIn"),
     GITHUB("Профиль GitHub"),
     STACKOVERFLOW("Профиль StackOverflow"),
@@ -21,5 +31,14 @@ public enum ContactType {
 
     public String getTitle() {
         return title;
+    }
+
+    public String toHtml0(String value) {
+        return title + ": " + value;
+    }
+
+    public String toHtml(String value) {
+        return (value == null) ? "" : toHtml0(value);
+
     }
 }
