d379fbe5eb4a2d65b94474c026355f88dd788e6e
Pdkhvtln <andrey.podkhvatilin@ya.ru>
2017-03-19 13:33:20 +0200

    The file view.jsp was fixed. And itroduce new class HtmlUtil for fomating date in html. In class DateUtil introduce new method format. In the file style.css added  tag <h3>.

--- src/com/urise/webapp/utils/DateUtil.java
+++ src/com/urise/webapp/utils/DateUtil.java
@@ -2,15 +2,21 @@ package com.urise.webapp.utils;
 
 import java.time.LocalDate;
 import java.time.Month;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Created by andrew on 08.02.17.
  */
 public class DateUtil {
     public static final LocalDate NOW = LocalDate.of(3000, 1, 1);
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("MM/yyyy");
 
     public static LocalDate of(int year, Month month) {
         return LocalDate.of(year, month, 1);
     }
 
+    public static String format(LocalDate date) {
+        if (date == null) return "";
+        return date.equals(NOW) ? "Сейчас" : date.format(DATE_FORMATTER);
+    }
 }
